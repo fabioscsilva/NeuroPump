37f41ac90fb1914fdb629ba454018da8f13125b8
scrodrigues <srodrigues14@gmail.com>
2012-12-05 10:59:15 +0000

    Adicionado a foto que é selecionada por defeito

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -6,7 +6,7 @@ class Patient < ActiveRecord::Base
   belongs_to :login
   has_many :sessions
   has_many :clinical_histories
-  has_attached_file :photo
+  has_attached_file :photo, :default_url => "/assets/noimage.jpg"
 
   attr_accessible :active, :address, :date_of_birth, :identification_code, :mobilephone, :name, :photo, :telephone, :education, :profession, :email, :palavrapass
   attr_accessor :email, :palavrapass
--- app/models/secretary.rb
+++ app/models/secretary.rb
@@ -3,7 +3,7 @@ class Secretary < ActiveRecord::Base
   belongs_to :gender
   belongs_to :login
   has_many :sessions
-  has_attached_file :photo
+  has_attached_file :photo, :default_url => "/assets/noimage.jpg"
 
   attr_accessible :active, :address, :date_of_birth, :identification_code, :mobilephone, :name, :photo, :telephone
 end
