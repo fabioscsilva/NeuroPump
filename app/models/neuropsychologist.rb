47ad6dea0f55df8a678b44ac45d26a9af5f8fa7c
Fabio Silva <pg19834@alunos.uminho.pt>
2012-12-05 09:59:57 +0000

    New Manager Features

--- app/models/neuropsychologist.rb
+++ app/models/neuropsychologist.rb
@@ -4,5 +4,7 @@ class Neuropsychologist < ActiveRecord::Base
   belongs_to :login
   has_many :sessions
   
-  attr_accessible :active, :address, :date_of_birth, :identification_code, :mobilephone, :name, :photo, :telephone
+  attr_accessible :active, :address, :date_of_birth, :identification_code, :mobilephone, :name, :photo, :telephone, :login_attributes
+  
+  accepts_nested_attributes_for :login
 end
