d446d3591562f354fb08074ada200a411fc26750
Fabio Silva <pg19834@alunos.uminho.pt>
2012-12-05 15:33:43 +0000

    Production Changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ NeuroPump::Application.configure do
   config.assets.compress = true
 
   # Precompile André
-  config.assets.precompile = ['*.js', '*.css']
+  config.assets.precompile = ['*.js', '*.css', '*.png', '*.jpeg', '*.jpg']
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
