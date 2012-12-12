ab6a5f3dabd8ca8e6cc3c34d4932d32b6a0ffd83
Fabio Silva <pg19834@alunos.uminho.pt>
2012-12-12 11:10:08 +0000

    Database Changes

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -5,9 +5,5 @@ class Administrator < ActiveRecord::Base
   attr_accessible :active, :address, :mobilephone, :name, :telephone, :login_id
   
   validates_presence_of :name, :active, :address
-  
-  validates_presence_of :name, :active, :address, :email, :password
-  validates :email,:format => {
-                             :with    => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i,
-                             :message => "formato incorreto" }
+
 end
