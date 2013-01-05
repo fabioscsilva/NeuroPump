b6daab5cd4e8478ea3fcbcf5d68a6e4bbc564583
Fabio Silva <pg19834@alunos.uminho.pt>
2013-01-05 10:06:43 +0000

    Add Agenda Secretary

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,4 +7,9 @@ class Appointment < ActiveRecord::Base
   has_many :appointment_plans
 
   attr_accessible :description, :appointment_day, :patient_id, :secretary_id, :neuropsychologist_id, :appointment_status_id, :appointment_type_id
+
+  scope :is_active, where(:active => true)
+  scope :in_clinic, lambda { |clinic_id| 
+    where(:clinic_id => clinic_id)
+  }
 end
