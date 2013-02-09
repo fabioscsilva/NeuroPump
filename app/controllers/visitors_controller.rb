ac7595e9dcdf456c622b755ded5883ce3fb0ebd3
prodrigues <prdrodrigues@gmail.com>
2013-02-09 12:38:38 +0000

    Página Corporativa finalizada Página de preçário finalizada

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -6,6 +6,12 @@ class VisitorsController < ApplicationController
   end
   
   def pricing
+  	 @packages = Package.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @packages }
+    end
   end
   
   def contact
